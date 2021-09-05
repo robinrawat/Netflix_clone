da97bb3c58862234d7ef4abe86530ecf993750ea
Robin Rawat <robinrawat97@gmail.com>
2021-09-05 08:20:14 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Movies } from './models/movies';
 import { MovieService } from './services/movie.service';
